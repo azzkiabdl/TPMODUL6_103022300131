cfbfff0040ed7225fb2453ee63bc6b15e2161f18
azzkiabdl <147375620+azzkiabdl@users.noreply.github.com>
2025-03-23 23:54:16 +0700

    menambahkan class SayaTubeVideo

--- Program.cs
+++ Program.cs
@@ -1,2 +1,44 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Diagnostics;
+
+public class SayaTubeVideo
+{
+    private int id;
+    private string title;
+    private int playCount;
+
+    public SayaTubeVideo(string title)
+    {
+        Random random = new Random();
+        this.id = random.Next(10000, 99999); 
+        this.title = title;
+        this.playCount = 0; 
+    }
+
+    public void IncreasePlayCount(int count)
+    {
+        Debug.Assert(count > 0 && count <= 10000000, "Jumlah play count harus antara 1 dan 10.000.000");
+
+        try
+        {
+            checked
+            {
+                this.playCount += count;
+            }
+        }
+        catch (OverflowException)
+        {
+            Console.WriteLine("Terjadi overflow saat menambah play count!");
+        }
+    }
+
+    public void PrintVideoDetails()
+    {
+        Console.WriteLine("==========================");
+        Console.WriteLine($"ID: {this.id}");
+        Console.WriteLine($"Title: {this.title}");
+        Console.WriteLine($"Play Count: {this.playCount}");
+        Console.WriteLine("==========================");
+    }
+}
+
